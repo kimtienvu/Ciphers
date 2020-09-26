ccdf13e1582bba49a480b8d2dc13e990c02a93bd
Kim Tien Vu <45075049+kimtienvu@users.noreply.github.com>
2020-09-26 14:04:32 -0700

    Setting up constants

--- C_Cipher.c
+++ C_Cipher.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //Global variables
 int DEFAULT = 17;
 int LENGTH = 100;
+char * LITERAL = "%s\n";
+char * PROMPT = "Insert Message to Encrypt... ";
+char * CHOOSE = "Would you like to choose the key? (y/n) ";
+char * ERROR = "No input/invalid input";
 int key = 0;
 
+
 // Method to run the Caesar Cipher
-encrypt(string userInput, int cipherKey)
+char * encrypt(char * userInput, int cipherKey)
 {
     // 
 }
@@ -16,15 +23,30 @@ int main()
 {
     // 99 characters is plenty for a message right?
     char message[LENGTH]; //NOTE: Need 1 extra character for the end of string!!!!
+    char choice[1];
     
     // Get user input for the message to encrypt
-    print("Insert Message to Encrypt...");
-    //char input = fgets(message, LENGTH, stdin);
+    printf(PROMPT);
+    char * input = fgets(message, LENGTH, stdin);
+    
+    printf(input == NULL);
+    
+    // Check if input is empty
+    /*
+    if(input[0] == '\0') {
+        
+        printf(LITERAL, ERROR);
+        exit(0);
+    }
+    printf(LITERAL, input); 
+    
     
     //Prompt if they wish to choose the key or if program should choose default key
-    print("Would you like to choose the key? (y/n)");
-    //char choice = y;//y/n would be stored here
-    
+    printf(CHOOSE);
+    char * y_n = fgets(choice, 1, stdin);
+    printf(LITERAL, y_n);
+
+    /*
     //Make sure y/n is not case sensitive! 
     choice = tolower(choice);
     
@@ -46,13 +68,14 @@ int main()
     }
     
     //Run the cipher 
-    char result = encrypt(input, key);
+    char * result = encrypt(input, key);
     
     //Display before encrypted Message
     print(input);
     
     //Display encrypted message
     print(result);
-
+*/
     return 0;
 }
+
