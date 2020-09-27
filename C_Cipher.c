3b69e7ead1ed95aaffe2df498e93be9f3d6c27c7
Kim Tien Vu <45075049+kimtienvu@users.noreply.github.com>
2020-09-27 13:49:16 -0700

    C Cipher only works with positive integers

--- C_Cipher.c
+++ C_Cipher.c
@@ -1,81 +1,105 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define  DEFAULT 17
+#define  MAX_ARG 3
+#define  NO_KEY_ARG 2
+#define  KEY_ARG 2
+#define  LENGTH 100
+#define  SIZE 2
+#define  LITERAL "%s\n"
+#define  PROMPT "Insert Message to Encrypt... "
+#define  CHOOSE "Would you like to choose the key? (y or n) "
+#define  ERROR "No input or invalid input"
+#define  Y_CHAR "y"
+#define  N_CHAR "n"
+#define  BEFORE "Original message: %s\n"
+#define  AFTER "Encrypted message: "
+
 //Global variables
-int DEFAULT = 17;
-int LENGTH = 100;
-char * LITERAL = "%s\n";
-char * PROMPT = "Insert Message to Encrypt... ";
-char * CHOOSE = "Would you like to choose the key? (y/n) ";
-char * ERROR = "No input/invalid input";
 int key = 0;
 
-
 // Method to run the Caesar Cipher
-char * encrypt(char * userInput, int cipherKey)
+void encrypt(char * userInput, int cipherKey)
 {
-    // 
+  char encrypted_text[] = "";
+  char letter;
+  int ascii;
+
+  // Rotate the characters by the cipherKey
+  for(int i = 0; i < strlen(userInput) - 1; i++) {
+    
+    ascii = (int) userInput[i];
+    ascii += cipherKey;
+    letter = (char) ascii;
+    printf("%c", letter);
+  }
 }
 
 // Run main program here
-int main()
+int main(int argc, char * argv[])
 {
-    // 99 characters is plenty for a message right?
-    char message[LENGTH]; //NOTE: Need 1 extra character for the end of string!!!!
-    char choice[1];
+  // Checks the number of arguments (max is 3)
+  // Name of program, y (if user wants to choose a key), key
+  if(argc == MAX_ARG) {
     
-    // Get user input for the message to encrypt
-    printf(PROMPT);
-    char * input = fgets(message, LENGTH, stdin);
-    
-    printf(input == NULL);
-    
-    // Check if input is empty
-    /*
-    if(input[0] == '\0') {
+    // Check if 2nd argument is not numeric (y)
+    char check_letter = (char) tolower(*argv[1]);
+    if(isdigit(*argv[1]) == 0  && strncmp(&check_letter, Y_CHAR, 1) == 0) {
         
-        printf(LITERAL, ERROR);
-        exit(0);
-    }
-    printf(LITERAL, input); 
-    
-    
-    //Prompt if they wish to choose the key or if program should choose default key
-    printf(CHOOSE);
-    char * y_n = fgets(choice, 1, stdin);
-    printf(LITERAL, y_n);
-
-    /*
-    //Make sure y/n is not case sensitive! 
-    choice = tolower(choice);
-    
-    //If users would like to choose the key (they put "y")
-    if(strcmp(choice, "y") == 0) 
-    {
-        //Prompt user for number, Make sure that the key is an integer!
-        //TODO: I think positive or negative integers are okay, need to test them!!!
-        print("Please choose an integer for the key");
-        //int chooseKey = fgets()
-        //Check if the input is an integer!
-        //Store this integer as a key
-        
+      // Check if user inputted an integer for the key (not other invalid argument)
+      if(isdigit(*argv[2]) != 0) {
+        key = atoi(argv[2]);
+      }
     } 
-    else
-    {
-        print("Default key will be used...");
-        key = DEFAULT;
+    else {
+      printf(LITERAL, ERROR);
+      exit(1);
     }
+  } 
+  // Name of program, n (user does not want to choose a key) 
+  else if (argc == NO_KEY_ARG) {
+      
+    // Check if user inputted n and use the default key
+    char check_letter = (char) tolower(*argv[1]);
+    if(isdigit(*argv[1]) == 0 && strncmp(&check_letter, N_CHAR, 1) == 0) {
+        
+      key = DEFAULT;
+    } 
+    else {
+      printf(LITERAL, ERROR);
+      exit(1);
+    }
+  }
+  // Invalid/only program name, exit the program
+  else {
+    printf(LITERAL, ERROR);
+    exit(1);
+  }
+   
+  //Use fgets to get user input message to encrypt
+  char message[LENGTH];
+       
+  printf(PROMPT);
+  char * input = fgets(message, LENGTH, stdin);
+  
+  // Exit the program if no message
+  if(input == NULL || strlen(input) == 1) {
+    printf(LITERAL, ERROR);
+    exit(1);
+  }
+
+  //Display before encrypted Message
+  printf(BEFORE, input);
+  
+  printf("The key is: %d\n", key);
     
-    //Run the cipher 
-    char * result = encrypt(input, key);
-    
-    //Display before encrypted Message
-    print(input);
-    
-    //Display encrypted message
-    print(result);
-*/
-    return 0;
+  //Display encrypted message
+  printf(AFTER);
+  encrypt(input, key);
+  
+  return 0;
 }
 
